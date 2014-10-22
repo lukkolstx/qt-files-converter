1a6eda8be4044a7a2023010e8adbb11ee9b7c011
Łukasz Kolat <lukaszkolat@gmail.com>
2014-10-22 15:08:27 +0200

    Functionality added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='qt_files_converter',
+      version='0.1',
+      description='Converts .qt files',
+      author='No name',
+      author_email='noname@example.com',
+      license='free',
+      packages=['qt_files_converter'],
+      zip_safe=True)
